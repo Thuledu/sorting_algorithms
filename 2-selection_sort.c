ac915f0981267aa0196c9d2dd4eb209aaa7e8a64
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-19 14:20:50 -0800

    Sorting algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,26 +3,11 @@
 #include "sort.h"
 
 
-void swap(int *a, int *b);
-void selection_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-
-int main(void)
-{
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-	size_t n = sizeof(array) / sizeof(array[0]);
-
-	print_array(array, n);
-	printf("\n");
-
-	selection_sort(array, n);
-
-	printf("\n");
-	print_array(array, n);
-
-	return (0);
-}
-
+/**
+ * swap - Swaps 2 integers in an array.
+ * @a: first integer 
+ * @b: second integer
+ */
 void swap(int *a, int *b)
 {
 	int temp = *a;
@@ -38,37 +23,23 @@ void swap(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	for (size_t i = 0; i < size - 1; i++)
-	{
-		size_t min_index = i;
+    size_t i, j, min_idx;
+    int temp;
 
-		for (size_t j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_index])
-			{
-				min_index = j;
-			}
-		}
-
-		if (min_index != i)
-		{
-			swap(&array[i], &array[min_index]);
-
-			for (size_t k = 0; k < size; k++)
-			{
-				printf("%d ", array[k]);
-			}
-
-			printf("\n");
-		}
-	}
-}
-
-void print_array(const int *array, size_t size)
-{
-	for (size_t i = 0; i < size; i++)
-	{
-		printf("%d ", array[i]);
-	}
-	printf("\n");
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
+            print_array(array, size);
+        }
+    }
 }
