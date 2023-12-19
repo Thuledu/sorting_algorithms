afbcd2b64596cb1750da5de9b0fb387df8ccde32
Thuledu Tshabalala <thuli.tshabalala98@gmail.com>
2023-12-19 15:17:09 -0800

    Sorting algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,81 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
-void shell_sort(int *array, size_t size);
-int main(void);
-
 /**
- * shell_sort - Sorts an array of integers in
- * ascending order using the Shell sort algorithm
- * @array: The array to sort
- * @size: The size of the array
+ * swap - Swaps two elements in an array
+ * @a: Pointer to the first element
+ * @b: Pointer to the second element
  */
-void shell_sort(int *array, size_t size)
+void swap(int *a, int *b)
 {
-	/* Generate the Knuth sequence */
-	int gap = 1;
-	while (gap <= (int)size / 3)
-	{
-		gap = gap * 3 + 1;
-	}
-
-	/* Perform the Shell sort */
-	while (gap > 0)
-	{
-		int i;
-		for (i = gap; i < (int)size; i++)
-		{
-			int temp = array[i];
-			int j = i;
-
-			while (j >= gap && array[j - gap] > temp)
-			{
-				array[j] = array[j - gap];
-				j -= gap;
-			}
-
-			array[j] = temp;
-		}
-
-		/* Print the array after each iteration */
-		int j;
-		for (j = 0; j < (int)size; j++)
-		{
-			printf("%d ", array[j]);
-		}
-
-		printf("\n");
-
-		gap = (gap - 1) / 3;
-	}
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 /**
- * main - Entry point
- * Return: Always 0
+ * shell_sort - Sorts an array of integers in ascending order
+ *              using the Shell sort algorithm with Knuth sequence
+ * @array: Pointer to the array to be sorted
+ * @size: Size of the array
  */
-int main(void)
+void shell_sort(int *array, size_t size)
 {
-	int array[] = {9, 5, 7, 1, 3};
-	size_t size = sizeof(array) / sizeof(array[0]);
+    size_t gap = 1, i, j;
+    int temp;
 
-	int i;
-	for (i = 0; i < (int)size; i++)
-	{
-		printf("%d ", array[i]);
-	}
+    if (array == NULL || size < 2)
+        return;
 
-	printf("\n");
+    while (gap < size / 3)
+        gap = gap * 3 + 1;
 
-	shell_sort(array, size);
-
-	int j;
-	for (j = 0; j < (int)size; j++)
-	{
-		printf("%d ", array[j]);
-	}
-	printf("\n");
-
-	return (0);
+    while (gap > 0)
+    {
+        for (i = gap; i < size; i++)
+        {
+            temp = array[i];
+            j = i;
+            while (j >= gap && array[j - gap] > temp)
+            {
+                array[j] = array[j - gap];
+                j -= gap;
+            }
+            array[j] = temp;
+        }
+        print_array(array, size);
+        gap = (gap - 1) / 3;
+    }
 }
